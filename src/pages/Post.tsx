7402fb6b290daf235ed3eecef09456905dd8a8bf
Pablo Martins <pablomartins1515@hotmail.com>
2023-05-15 09:22:45 -0300

    Add files via upload

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -11,14 +11,14 @@ export function Post () {
         <div className="flex flex-col min-h-screen">            
             <Header />
 
-            {/*<main className="flex flex-1">
+            <main className="flex flex-1">
                
                 {slug ? 
                     <MainBlog lessonSlug={slug} />
                      : <div className="flex-1  text-2xl text-zinc-200 text-opacity-30   hover:mix-blend-hard-light font-bold flex 
                      gap-2 items-center justify-center " >Clique em uma aula <ArrowRight size={32}/></div>  }               
                 
-                </main>*/}
+                </main>
 
             <Rodape />                       
         </div>   
